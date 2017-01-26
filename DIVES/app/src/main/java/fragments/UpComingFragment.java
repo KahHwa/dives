dd63e9917bbb9ca3ef39eb6735a245e1e54a537b
Rico Ardiya <ricoardiya@yahoo.com>
2017-01-27 00:14:49 +0800

    adding validate form

--- DIVES/app/src/main/java/fragments/UpComingFragment.java
+++ DIVES/app/src/main/java/fragments/UpComingFragment.java
@@ -19,7 +19,7 @@ public class UpComingFragment extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        View rootView = inflater.inflate(R.layout.fragment_message,container,false);
+        View rootView = inflater.inflate(R.layout.fragment_upcoming,container,false);
 
         return rootView;
     }
