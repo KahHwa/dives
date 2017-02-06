4bb4899f3f9995241744d72ee8d0da978c7949b6
Rico Ardiya <ricoardiya@yahoo.com>
2017-02-06 22:54:50 +0800

    adding details of dashboard

--- DIVES/app/src/main/java/fragments/ProfileFragment.java
+++ DIVES/app/src/main/java/fragments/ProfileFragment.java
@@ -1,8 +1,11 @@
 package fragments;
 
 import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,6 +19,8 @@ import com.example.ricoardiya.dives.R;
 
 public class ProfileFragment extends Fragment {
 
+    Button button;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -23,18 +28,26 @@ public class ProfileFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_profile,container,false);
 
 
-        Button button = null;
-        button = (Button) button.findViewById(R.id.bEditProfile);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                // Do something in response to button click
+//        Button button = null;
 
-                android.app.FragmentManager fn = getFragmentManager();
+//        button = (Button) button.findViewById(R.id.bEditProfile);
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                // Do something in response to button click
+//
+//                Fragment newFragment = new EditProfileFragment();
+//                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+//
+//                transaction.replace(R.id.content_frame, newFragment);
+//                transaction.addToBackStack(null);
+//
+//                transaction.commit();
+//
+//            }
+//        });
 
-                fn.beginTransaction().replace(R.id.content_frame, new EditProfileFragment()).commit();
 
-            }
-        });
         return rootView;
 
 
