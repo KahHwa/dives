7e6eec0cbfaf02a557e3d435104e0355108b1a36
Rico Ardiya <ricoardiya@yahoo.com>
2017-02-22 23:02:12 +0800

    adding php

---
+++ Web DIVES/php/logincompany.php
@@ -0,0 +1,13 @@
+<html>
+    <body>
+        <?php 
+            $email= $_GET['email'];
+
+            echo "$email";
+        ?>
+        <a href="../html/dashboard.html">Click Here to Continue</a>
+    </body>
+    
+    
+</html>
+
