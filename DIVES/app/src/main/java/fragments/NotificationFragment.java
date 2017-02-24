4e6919a6bbad25ba1d257c543afcfa39abb8dc25
Rico Ardiya <ricoardiya@yahoo.com>
2017-02-25 00:23:33 +0800

    add cardview

--- DIVES/app/src/main/java/fragments/NotificationFragment.java
+++ DIVES/app/src/main/java/fragments/NotificationFragment.java
@@ -3,6 +3,8 @@ package fragments;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,12 +17,19 @@ import com.example.ricoardiya.dives.R;
 
 public class NotificationFragment extends Fragment {
 
+    RecyclerView recyclerView;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.fragment_notification,container,false);
 
+        recyclerView = (RecyclerView) recyclerView.findViewById(R.id.recycler);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        recyclerView.setAdapter(new RecyclerAdapter());
         return rootView;
     }
 }
