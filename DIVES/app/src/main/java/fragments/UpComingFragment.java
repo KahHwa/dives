16723eb1d8077fcf8aa1389b37f6049b1fbfb0d3
Rico Ardiya <ricoardiya@yahoo.com>
2017-02-27 20:31:59 +0800

    changing upcoming fragment

--- DIVES/app/src/main/java/fragments/UpComingFragment.java
+++ DIVES/app/src/main/java/fragments/UpComingFragment.java
@@ -6,8 +6,13 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.example.ricoardiya.dives.R;
+
+import static com.example.ricoardiya.dives.R.layout.fragment_notification;
+import static com.example.ricoardiya.dives.R.layout.fragment_upcoming;
 
 /**
  * Created by Rico Ardiya on 1/25/2017.
@@ -15,11 +20,21 @@ import com.example.ricoardiya.dives.R;
 
 public class UpComingFragment extends Fragment{
 
-    @Nullable
+    // Array of strings...
+    public String[] mobileArray = {"job fair by Nanyang Technological University ",
+            "Seminar by Microsoft"
+
+    };
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(fragment_upcoming,container,false);
 
-        View rootView = inflater.inflate(R.layout.fragment_upcoming,container,false);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), R.layout.upcoming_listview, R.id.label_upcoming, mobileArray);
+
+
+        ListView listView = (ListView) rootView.findViewById(R.id.upcoming_list);
+        listView.setAdapter(adapter);
 
         return rootView;
     }
